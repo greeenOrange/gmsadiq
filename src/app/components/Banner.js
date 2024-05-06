61dc80afc0501e1860d0ee81dccbcc57b6bce376
greeenOrange <gm1sadiq@gmail.com>
2024-05-06 15:13:16 +0600

    contacttext

--- src/app/components/Banner.js
+++ src/app/components/Banner.js
@@ -31,7 +31,7 @@ function Banner() {
                 if (updatedThisRound.get() === true && latest > 0) {
                     updatedThisRound.set(false);
                 } else if (updatedThisRound.get() === false && latest === 0) {
-                    if (textIndex.get() === texts.length - 1) {
+                    if (textIndex.get() === texts?.length - 1) {
                         textIndex.set(0);
                     } else {
                         textIndex.set(textIndex.get() + 1);
@@ -40,13 +40,13 @@ function Banner() {
                 }
             }
         });
-    }, []);
+    }, [textIndex, texts, count, updatedThisRound, texts?.length]);
 
     return (
         <div className="h-64 md:h-96 w-full bg-cover bg-center" style={{ backgroundImage: "url('https://greeenorange.github.io/Portfolio-website/images/hero-bg.svg')" }}>
             <div className="absolute inset-0 flex items-center justify-center">
                 <div className="text-center">
-                <p className="text-lg md:text-xl text-dark mt-4 text-gray-900 font-bold">I'm gm sadiq</p>
+                <p className="text-lg md:text-xl text-dark mt-4 text-gray-900 font-bold">{"I'm gm sadiq"}</p>
                     <h1 className="font-bold text-gray-900 md:text-5xl Class
 Properties"> <span className="block">a </span><motion.span className='inline uppercase text-transparent bg-gradient-to-r from-purple-600 via-red-600 to-yellow-400 bg-clip-text'>
                             {displayText}</motion.span>
