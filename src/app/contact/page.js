be11bad011444545c6b3135e95450b518f004971
greeenOrange <gm1sadiq@gmail.com>
2024-05-06 17:49:34 +0600

    errorsolved

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -2,6 +2,9 @@
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import { GoDotFill } from "react-icons/go";
+import Image from 'next/image';
+import { SlEnvolope } from "react-icons/sl";
+import { MdOutlinePhone } from "react-icons/md";
 
 const Contact = () => {
 
@@ -60,26 +63,41 @@ const Contact = () => {
   return (
     <section className="py-8">
       <div className="container mx-auto">
-        <h2 className="text-4xl font-semibold mb-8 text-center">
-          <span className='inline text-transparent bg-gradient-to-r from-purple-600 via-red-600 to-yellow-400 bg-clip-text leading-snug'>Let’s Connect</span>
+        <div className="flex gap-2 items-center justify-center">
+        <h2 className="text-4xl font-semibold text-center">
+          <span className='inline text-transparent bg-gradient-to-r from-purple-600 via-red-600 to-yellow-400 bg-clip-text leading-snug'>Let’s Connect
+          </span>
         </h2>
+        <Image
+          src={`https://oshinewptheme.com/v59/wp-content/uploads/sites/66/2024/01/contact.svg`}
+          width={50}
+          height={50} 
+          alt='contact us'
+          />
+        </div>
+        <p className="text-center py-4 mb-8">Don’t need be Shy, Write to me</p>
         <div className="flex items-center justify-between gap-8">
-          <p className="bg-[#f0ffc8] px-4 py-2 rounded-full">gm1sadiq@gmail.com</p>
+          <p className="bg-[#f0ffc8] px-4 py-2 rounded-full flex items-center justify-center gap-1">
+          <SlEnvolope />
+            gm1sadiq@gmail.com
+            </p>
           <motion.div
             className="bottom-4 right-4"
             animate={{ y: [-10, 0, -10], transition: { repeat: Infinity, duration: 1 } }}
           >
             <GoDotFill className="bg-black rounded-full" />
           </motion.div>
-          <p className="bg-[#ffaeae] px-4 py-2 rounded-full">+8801515649932</p>
+          <p className="bg-[#ffaeae] px-4 py-2 rounded-full flex items-center justify-center gap-1">
+          <MdOutlinePhone />
+            +8801515649932
+            </p>
         </div>
 
-        <form onSubmit={handleSubmit} className="max-w-md mx-auto">
+        <form onSubmit={handleSubmit} className="max-w-md mx-auto mt-10">
       <div className="mb-4">
-        <label htmlFor="name" className="block font-medium text-gray-700">Name</label>
         <input
           type="text"
-          id="name"
+          placeholder="full name"
           name="name"
           value={formData.name}
           onChange={handleChange}
@@ -88,11 +106,10 @@ const Contact = () => {
         />
       </div>
       <div className="mb-4">
-        <label htmlFor="email" className="block font-medium text-gray-700">Email</label>
         <input
           type="email"
-          id="email"
           name="mail"
+          placeholder="your email"
           value={formData.mail}
           onChange={handleChange}
           required
@@ -100,11 +117,10 @@ const Contact = () => {
         />
       </div>
       <div className="mb-4">
-        <label htmlFor="subject" className="block font-medium text-gray-700">Subject</label>
         <input
           type="text"
-          id="subject"
           name="subject"
+          placeholder="subject"
           value={formData.subject}
           onChange={handleChange}
           required
@@ -112,9 +128,8 @@ const Contact = () => {
         />
       </div>
       <div className="mb-4">
-        <label htmlFor="message" className="block font-medium text-gray-700">Message</label>
         <textarea
-          id="message"
+          placeholder="write message..."
           name="message"
           value={formData.message}
           onChange={handleChange}
